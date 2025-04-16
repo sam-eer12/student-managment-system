acefb830414864f0228e095ac99fb1305399e5f6
Sameer Gupta <sameer.gupta.ug24@nsut.ac.in>
2025-04-16 21:24:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def create_connection():
             database='project'
         )
         return connection
+
     except Error as e:
         print(f"Error connecting to MySQL: {e}")
         return None
